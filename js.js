f03c69e4c86f866e6599cd3a8be8cb22f0d7d5a9
LISANUDEEN N <111849988+rootintootinwar@users.noreply.github.com>
2022-12-30 00:22:46 +0530

    Add files via upload

--- js.js
+++ js.js
@@ -9,6 +9,7 @@ function show_pass(){
 		document.getElementById("pass").setAttribute("type","text");
 		state = true;
 	}
+	
 	if(statee){
 		document.getElementById("rpass").setAttribute("type","password");
 		statee = false;
@@ -19,3 +20,22 @@ function show_pass(){
 	}
 }
 
+function pass_strength(password){
+			let i = 0;
+			if(password.length > 6){
+				i++;
+			}
+			if(password.length >= 10){
+				i++;
+			}
+			if(/[A-Z]/.test(password)){
+				i++;
+			}
+			if(/[0-9]/.test(password)){
+				i++;
+			}
+			if(/[A-Za-z0-8]/.test(password)){
+				i++;
+			}
+			return i;
+		}
