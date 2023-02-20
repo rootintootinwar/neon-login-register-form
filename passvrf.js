84a0df80694a8015e6ed7310cd816249eb2fbb60
LISANUDEEN N <111849988+rootintootinwar@users.noreply.github.com>
2023-02-20 22:37:40 +0530

    Add files via upload

--- passvrf.js
+++ passvrf.js
@@ -1,3 +1,46 @@
+var state = false;
+var statee = false;
+function show_pass(){
+	if(state){
+		document.getElementById("pass").setAttribute("type","password");
+		state = false;
+	}
+	else{
+		document.getElementById("pass").setAttribute("type","text");
+		state = true;
+	}
+	
+	if(statee){
+		document.getElementById("rpass").setAttribute("type","password");
+		statee = false;
+	}
+	else{
+		document.getElementById("rpass").setAttribute("type","text");
+		statee = true;
+	}
+}
+
+function pass_strength(password){
+			let i = 0;
+			if(password.length > 6){
+				i++;
+			}
+			if(password.length >= 10){
+				i++;
+			}
+			if(/[A-Z]/.test(password)){
+				i++;
+			}
+			if(/[0-9]/.test(password)){
+				i++;
+			}
+			if(/[A-Za-z0-8]/.test(password)){
+				i++;
+			}
+			return i;
+}
+
+
 function pfun(){
 	var email = document.forms["epform"]["uname"].value;
 	var epattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
